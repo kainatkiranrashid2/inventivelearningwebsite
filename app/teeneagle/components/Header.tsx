39866134b270dfc434c3097197650c0702952555
kainat <kainatkiranrashid2@gmail.com>
2025-08-18 18:37:30 +0500

    `Update Header component: adjust height and typography`

--- app/teeneagle/components/Header.tsx
+++ app/teeneagle/components/Header.tsx
@@ -4,13 +4,12 @@ const Header = () => {
   return (
     <section className="bg-[#AFDDE5]">
       <div className="container">
-        <div className="flex flex-col items-center justify-center h-[400px] md:h-screen text-center px-4">
+        <div className="flex flex-col items-center justify-center h-[500px] md:h-screen text-center px-4">
           <h1
-            className="text-[32px] sm:text-[40px] md:text-[44px] lg:text-[48px] xl:text-[56px] 2xl:text-[64px] 
-            text-[#003135] font-bold">
+            className="text-[32px] sm:text-[40px] md:text-[46px] lg:text-[50px] xl:text-[54px] 2xl:text-[60px] 
+            text-[#003135] font-bold md:max-w-2xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl">
             Pakistan&apos;s Future is Taking Flight
           </h1>
-
           <h3
             className="mt-4 text-[20px] sm:text-[26px] md:text-[32px] lg:text-[36px] xl:text-[40px] 2xl:text-[44px] 
             text-[#024950] font-medium md:max-w-3xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl">
